ce7aee88454f882c47be39fd91592db90951626c
Mohamedemad456 <mohamed.asker316@gmail.com>
2025-12-13 22:10:44 +0200

    feat: add frontend and change the final borrowing and returning endpoints integration (#2)

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,8 @@
+// Role: UI Developer (Utility Functions)
+// Developer: Mohamed Emad
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
